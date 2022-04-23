4cab861c2be59f164a31c63047f49f82d9d1f1a9
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-04-23 16:58:43 +0200

    Vouting

--- contracts/Actions.sol
+++ contracts/Actions.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/Mintable.sol";
 
 contract Actions {
@@ -16,6 +17,7 @@ contract Actions {
         bytes metadata;
 
         uint256 amount;
+        bool settled;
     }
 
     struct Proof {
@@ -27,6 +29,11 @@ contract Actions {
         address creator;
         uint256 proofIndex;
         bytes disputeProof;
+        uint256 disputeEndDate;
+        bool settled;
+
+        uint256 forVotes;
+        uint256 againstVotes;
     }
 
     address governanceToken;
@@ -85,6 +92,69 @@ contract Actions {
         dispute.creator = msg.sender;
         dispute.proofIndex = proofIndex;
         dispute.disputeProof = proof;
+        dispute.disputeEndDate = block.timestamp + ONE_WEEK_IN_SECONDS;
         disputes[actionId].push(dispute);
+    }
+
+    function vote(uint256 actionId, uint256 disputeId, bool voteFor) public {
+        uint256 votingPower = IERC20(governanceToken).balanceOf(msg.sender);
+        require(votingPower > 0, "You can't vote without governance tokens");
+        // todo: prevent double voting
+        // todo: prevent transfer manipulation
+        Dispute storage dispute = disputes[actionId][disputeId];
+        if (voteFor) {
+            dispute.forVotes += votingPower;
+        } else {
+            dispute.againstVotes += votingPower;
+        }
+    }
+
+    function settle() public {
+        for (uint256 i = 0; i < actions.length; i++) {
+            Action memory a = actions[i];
+            if (a.settled) {
+                continue;
+            }
+            (bool ongoing, bool unsettled) = hasDisputes(i);
+            if (unsettled) {
+                // todo: settle disputes
+            }
+
+            if (!ongoing && block.timestamp > a.disputePeriodEnd) {// no ongoing disputes and dispute period ended settle action
+                // todo: settle action
+            }
+        }
+    }
+
+    function hasUnsettled() public view returns (bool)  {
+        for (uint256 i = 0; i < actions.length; i++) {
+            Action memory a = actions[i];
+            if (a.settled) {
+                continue;
+            }
+            // case one has unsettled disputes
+            (bool ongoing, bool unsettled) = hasDisputes(i);
+            if (unsettled || (!ongoing && block.timestamp > a.disputePeriodEnd)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // ongoing, unsettled
+    function hasDisputes(uint256 actionId) private view returns (bool, bool) {
+        Dispute[] memory actionDisputes = disputes[actionId];
+        bool ongoing = false;
+        bool unsettled = false;
+        for (uint256 i = 0; i < actionDisputes.length; i++) {
+            if (actionDisputes[i].disputeEndDate > block.timestamp) {
+                ongoing = true;
+            } else if (!actionDisputes[i].settled) {
+                unsettled = true;
+            }
+        }
+
+        return (ongoing, unsettled);
     }
 }
