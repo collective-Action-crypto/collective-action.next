b3c41ecbddb3cb5e9da09ff8a11a47dc34a68715
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-04-23 12:45:22 +0200

    Add submit and distribute functions

--- contracts/Action.sol
+++ contracts/Action.sol
@@ -1,41 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract Action {
 
+    uint256 public constant ONE_WEEK_IN_SECONDS = 604800;
+
     address public immutable creator;
-    IERC20 public immutable token;
     uint256 public immutable endDate;
     uint256 public immutable disputePeriod;
-    bytes32 public immutable image;
-    bytes32 public immutable metadata;
+    uint256 public immutable stakeAmount;
+    bytes public image;
+    bytes public metadata;
 
     uint256 public raisedAmount = 0;
-
     mapping(address => uint256) contributors;
+
+    address[] participants;
+    mapping(address => bytes) participantProofs;
 
     constructor(
         address _creator,
-        address _token,
         uint256 _endDate,
         uint256 _disputePeriod,
-        bytes32 _image,
-        bytes32 _metadata
+        uint256 _stakeAmount,
+        bytes memory _image,
+        bytes memory _metadata
     ) {
+        require(_endDate > block.timestamp, "_endDate can't be in the past");
+        require(_disputePeriod >= ONE_WEEK_IN_SECONDS, "_disputePeriod should be not less than a one week");
+
         creator = _creator;
-        token = IERC20(_token);
         endDate = _endDate;
         disputePeriod = _disputePeriod;
+        stakeAmount = _stakeAmount;
         image = _image;
         metadata = _metadata;
     }
 
-    function contribute(uint256 amount) public {
+    function contribute() public payable {
         require(block.timestamp <= endDate, "Contributions after end date are not allowed");
-        contributors[msg.sender] += amount;
-        raisedAmount += amount;
-        token.transferFrom(msg.sender, address(this), amount);
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
     }
+
+    function submitProof(bytes memory proof) public payable {
+        require(participantProofs[msg.sender].length == 0, "Can't submit a second proof");
+        require(msg.value == stakeAmount, "Can't add a proof as stake amount is not valid");
+
+        participants.push(msg.sender);
+        participantProofs[msg.sender] = proof;
+    }
+
+    function distribute() public {
+        require(block.timestamp >= endDate + disputePeriod, "Can't distribute money before dispute period ends");
+
+        uint256 amount = raisedAmount / participants.length + stakeAmount;
+        for (uint256 i = 0; i < participants.length; i++) {
+            payable(participants[i]).transfer(amount);
+        }
+    }
+
+    // todo: resolve dispute
+    // todo: raise dispute
 }
