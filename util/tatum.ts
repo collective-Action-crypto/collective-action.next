a54d8e492f769a64d85fa45996daf33ced0859be
Justin <jusheinz@aol.de>
2022-04-23 02:10:47 +0200

    integrations done

--- util/tatum.ts
+++ util/tatum.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_KEY = "f0f95c56-c616-4326-b0b9-d3715ed8233e";
-export async function callSmartContractFunction(
+export async function callSmartContractFunction( //would also call withdraw function from smart contract etc
   contractAddress: string,
   methodName: string,
   methodABI: object,
@@ -21,4 +21,22 @@ export async function callSmartContractFunction(
   });
   console.log(resp.data.txId);
   return resp.data.IpfsHash;
+}
+export async function pushToIPFS(file: string | Blob) {
+  //https://blog.tatum.io/tatum-partners-with-nft-storage-to-offer-free-ipfs-storage-for-nfts-to-developers-183dad64e79d
+  const url = `https://api-eu1.tatum.io/v3/ipfs`;
+  let data = new FormData();
+  data.append("file", file);
+  const resp = await axios.post(url, data, {
+    headers: {
+      "content-type": "application/json",
+      "x-api-key": API_KEY,
+    },
+  });
+  return resp.data.IpfsHash;
+}
+export async function getFromIPFS(cid: string) {
+  const url = `https://api-eu1.tatum.io/v3/ipfs/${cid}`;
+  const response = await axios.get(url);
+  return response.data;
 }
